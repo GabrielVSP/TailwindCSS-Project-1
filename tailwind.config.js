a247c8e2e943ee34308f9061682476ab1aab8976
Gabriel Vergílio <125078273+GabrielVSP@users.noreply.github.com>
2023-10-18 15:28:40 -0300

    Deployment

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    extend: {
+
+      flexBasis: {
+        '24/50': '48%',
+      },
+      width: {
+        '24/50': '48%',
+      }
+
+    },
+  },
+  plugins: [],
+}
+
